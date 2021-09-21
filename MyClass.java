096bec3868493918efc2b1900488be2c6ff0536a
Isha3006 <57089331+Isha3006@users.noreply.github.com>
2021-09-21 09:23:14 +0530

    Add files via upload

---
+++ MyClass.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+class MyClass{
+    public static void main(String args[]){
+       Scanner sc=new Scanner(System.in);
+       System.out.println("Enter basic salary");
+       int salary=sc.nextInt();
+       float tax=0;
+       float hra=0.15f;
+       float da=0.60f;
+       float totalsalary=0;
+       if(salary<=500000){
+           tax=0.0f;
+       }
+       else if(salary>500000 && salary<1000000)
+       {
+           tax=0.20f;
+       }
+       else{
+          tax=0.30f;
+       }
+       tax=salary*tax;
+       totalsalary=salary+(salary*hra)+(salary*da);
+       System.out.println(salary);
+}
+
+}
